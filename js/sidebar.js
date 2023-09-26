2df8cc254a59f2ce6992fc569e38ec348bcbf982
IE-Build <iebuild-bot[bot]@users.noreply.github.com>
2023-09-26 18:29:36 +0000

    Update project.yml NetAppDocs/netappdocs-build@b27d685534d513755fca4a8e4f44e472b95ada1c

--- js/sidebar.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function () {
-  $('#ie-phone-menu-button').click(function () {
-    $('#page').toggleClass('ie-phone-menu-open');
-    $('#ie-content-wrap').toggleClass('ie-phone-menu-open');
-  });
-});
